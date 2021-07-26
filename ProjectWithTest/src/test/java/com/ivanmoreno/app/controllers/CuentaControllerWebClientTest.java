89ef646b79946aa77098c8f78428eb4423d0a6d3
ivan <ivanmoreno405@gmail.com>
2021-07-26 11:38:51 -0500

    Listar and Guardar methods tested

--- ProjectWithTest/src/test/java/com/ivanmoreno/app/controllers/CuentaControllerWebClientTest.java
+++ ProjectWithTest/src/test/java/com/ivanmoreno/app/controllers/CuentaControllerWebClientTest.java
@@ -1,10 +1,12 @@
 package com.ivanmoreno.app.controllers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.hamcrest.Matchers;
@@ -82,6 +84,7 @@ class CuentaControllerWebClientTest {
 	}
 
 	@Test
+	@Order(3)
 	void testDetalle2() {
 		
 		client.get().uri("/api/cuentas/2")
@@ -93,6 +96,84 @@ class CuentaControllerWebClientTest {
 				Cuenta cuentaRespuesta = response.getResponseBody();
 				assertEquals("Carlos", cuentaRespuesta.getPersona());
 				assertEquals("60.00", cuentaRespuesta.getSaldo().toPlainString());
+			});
+	}
+	
+	@Test
+	@Order(4)
+	void testListar() {
+		
+		client.get().uri("/api/cuentas")
+		.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+				.jsonPath("$[0].persona").isEqualTo("Ivan")
+				.jsonPath("$[0].saldo").isEqualTo(150)
+				.jsonPath("$[1].persona").isEqualTo("Carlos")
+				.jsonPath("$[1].saldo").isEqualTo(60)
+				.jsonPath("$", Matchers.hasSize(2));
+				
+				
+	}
+	
+	@Test
+	@Order(5)
+	void testLista2() {
+		
+		client.get().uri("/api/cuentas")
+		.exchange()
+			.expectStatus().isOk()
+			.expectBodyList(Cuenta.class)
+			.consumeWith(response -> {
+				List<Cuenta> cuentas = response.getResponseBody();
+				
+				assertNotNull(cuentas);
+				assertEquals(2, cuentas.size());
+				assertEquals(1L, cuentas.get(0).getId());
+				assertEquals("Ivan", cuentas.get(0).getPersona());
+				assertEquals("150.0", cuentas.get(0).getSaldo().toPlainString());
+				assertEquals(2L, cuentas.get(1).getId());
+				assertEquals("Carlos", cuentas.get(1).getPersona());
+				assertEquals("60.0", cuentas.get(1).getSaldo().toPlainString());
+			});
+	}
+	
+	@Test
+	@Order(6)
+	void testGuardar() {
+		
+		Cuenta cuenta = new Cuenta(null, "Pedro", new BigDecimal("3000"));
+		
+		client.post().uri("/api/cuentas")
+		.contentType(MediaType.APPLICATION_JSON)
+		.bodyValue(cuenta)
+		.exchange()
+			.expectStatus().isCreated()
+			.expectBody()
+				.jsonPath("$.id").isEqualTo(3)
+				.jsonPath("$.persona").isEqualTo("Pedro")
+				.jsonPath("$.saldo").isEqualTo(3000);
+	}
+	
+	@Test
+	@Order(7)
+	void testGuardar2() {
+		
+		Cuenta cuenta = new Cuenta(null, "Pedra", new BigDecimal("4000"));
+		
+		client.post().uri("/api/cuentas")
+		.contentType(MediaType.APPLICATION_JSON)
+		.bodyValue(cuenta)
+		.exchange()
+			.expectStatus().isCreated()
+			.expectHeader().contentType(MediaType.APPLICATION_JSON)
+			.expectBody(Cuenta.class)
+			.consumeWith(response -> {
+				Cuenta cuentaRespuesta = response.getResponseBody();
+				assertNotNull(cuentaRespuesta);
+				assertEquals(4L, cuentaRespuesta.getId());
+				assertEquals("Pedra", cuentaRespuesta.getPersona());
+				assertEquals("4000", cuentaRespuesta.getSaldo().toPlainString());
 			});
 	}
 }
